2f6f18b99ac1e1e74ae3cb0010c93283a8c60138
Jeremy Forsythe <jeremyforsythe@pop-os.localdomain>
2024-04-09 12:20:19 -0700

    ML Package Install r File

---
+++ content/teaching/MachineLearningInR/packagesToInstall.r
@@ -0,0 +1,22 @@
+install.packages(c("caret",                 # Primary Machine Learning Package in R
+                   "adabag",                # For Random Forest Bagging
+                   "glmnet",                # For Generalized Linear Modeling 
+                   "neuralnet",             # For Neural Networks
+                   "e1071",                 # For Support Vector Machines
+                   "pls",                   # For Principal Component Regression
+                   "randomForest",          # For Random Forests
+                   "gbm",                   # For Gradient Boosting 
+                   "xgboost",               # For Extreme Gradient Boosting
+                   "shapr",                 # For Visualizations
+                   "PerformanceAnalytics",  # Assessing Model Performance 
+                   "RANN",                  # Data Preprocessing (Imputation)
+                   "purrr",                 # Extended Functionality For Processing Data
+                   "tidyr",                 # Extended Functionality For Processing Data
+                   "plotmo",                # For Visualizations
+                   "naniar",                # For Visualizations
+                   "UpSetR",                # For Visualizations
+                   "GGally",                # For Visualizations
+                   "ggplot2",               # For Visualizations
+                   "rattle"                 # For Visualizations
+                   )
+                 )
